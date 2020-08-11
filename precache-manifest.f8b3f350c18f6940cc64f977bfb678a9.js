298bdef7ae6021b38b27d0298363f421ea241ff5
DenisLay <56198059+DenisLay@users.noreply.github.com>
2020-08-11 19:05:34 +0300

    Updates

---
+++ precache-manifest.f8b3f350c18f6940cc64f977bfb678a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b40492abb196cf356fa354474b6cde2",
+    "url": "/FeedTheSnake/index.html"
+  },
+  {
+    "revision": "65ad75a1d07c4861f13e",
+    "url": "/FeedTheSnake/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5cd84aeee864e621b422",
+    "url": "/FeedTheSnake/static/js/2.831e4561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FeedTheSnake/static/js/2.831e4561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ad75a1d07c4861f13e",
+    "url": "/FeedTheSnake/static/js/main.986d93a6.chunk.js"
+  },
+  {
+    "revision": "5e4d6c321808d3348024",
+    "url": "/FeedTheSnake/static/js/runtime-main.a4e3e819.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FeedTheSnake/static/media/logo.5d5d9eef.svg"
+  }
+]);
